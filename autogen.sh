1de6dd90493ccb74bacf5dcfc21555b134dc090e
Sven Neumann <sven@gimp.org>
2004-06-15 17:08:07 +0000

    Makefile.am autogen.sh configure.in brushes/Makefile.am more or less
    2004-06-15  Sven Neumann  <sven@gimp.org>
    
    	* Makefile.am
    	* autogen.sh
    	* configure.in
    	* brushes/Makefile.am
    	* patterns/Makefile.am: more or less completely redone the build
    	setup.

--- autogen.sh
+++ autogen.sh
@@ -4,34 +4,66 @@ PROJECT="The GIMP Extra Data Files"
 TEST_TYPE=-f
 FILE="brushes/flower.gbr"
 
+AUTOCONF_REQUIRED_VERSION=2.54
+AUTOMAKE_REQUIRED_VERSION=1.6
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd $srcdir
 
 
+check_version ()
+{
+    if expr $1 \>= $2 > /dev/null; then
+	echo "yes (version $1)"
+    else
+	echo "Too old (found version $1)!"
+	DIE=1
+    fi
+}
+
+echo
+echo "I am testing that you have the required versions of autoconf and automake."
+echo
+
 DIE=0
 
-echo -n "checking for autoconf ... "
-(autoconf --version) < /dev/null > /dev/null 2>&1
-
-if [ $? -ne 0 ]; then
+echo -n "checking for autoconf >= $AUTOCONF_REQUIRED_VERSION ... "
+if (autoconf --version) < /dev/null > /dev/null 2>&1; then
+    VER=`autoconf --version \
+         | grep -iw autoconf | sed "s/.* \([0-9.]*\)[-a-z0-9]*$/\1/"`
+    check_version $VER $AUTOCONF_REQUIRED_VERSION
+else
     echo
     echo "  You must have autoconf installed to compile $PROJECT."
     echo "  Download the appropriate package for your distribution,"
-    echo "  or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+    echo "  or get the source tarball at ftp://ftp.gnu.org/pub/gnu/autoconf/"
     DIE=1;
 fi
 
-echo
-echo -n "checking for automake ... "
-(automake --version) < /dev/null > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+echo -n "checking for automake >= $AUTOMAKE_REQUIRED_VERSION ... "
+if (automake-1.7 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.7
+   ACLOCAL=aclocal-1.7
+elif (automake-1.8 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.8
+   ACLOCAL=aclocal-1.8
+elif (automake-1.6 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.6
+   ACLOCAL=aclocal-1.6
+else
     echo
-    echo "  You must have automake installed to compile $PROJECT."
-    echo "  Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.7.3.tar.gz"
-    echo "  (or a newer version if it is available)."
+    echo "  You must have automake 1.6 or newer installed to compile $PROJECT."
+    echo "  Download the appropriate package for your distribution,"
+    echo "  or get the source tarball at ftp://ftp.gnu.org/pub/gnu/automake/"
     DIE=1
+fi
+
+if test x$AUTOMAKE != x; then
+    VER=`$AUTOMAKE --version \
+         | grep automake | sed "s/.* \([0-9.]*\)[-a-z0-9]*$/\1/"`
+    check_version $VER $AUTOMAKE_REQUIRED_VERSION
 fi
 
 if test "$DIE" -eq 1; then
@@ -41,7 +73,6 @@ if test "$DIE" -eq 1; then
     exit 1
 fi
 
-
 test $TEST_TYPE $FILE || {
     echo
     echo "You must run this script in the top-level $PROJECT directory."
@@ -49,29 +80,64 @@ test $TEST_TYPE $FILE || {
     exit 1
 }
 
-
 if test -z "$*"; then
-    echo
-    echo "I am going to run ./configure with no arguments - if you wish "
-    echo "to pass any to it, please specify them on the $0 command line."
-    echo
+    if test -z "$AUTOGEN_CONFIGURE_ARGS"; then
+	echo
+	echo "I am going to run ./configure with no arguments - if you wish "
+	echo "to pass any to it, please specify them on the $0 command line "
+	echo "or set the AUTOGEN_CONFIGURE_ARGS environment variable."
+	echo
+    else
+	echo
+	echo "I am going to run ./configure with the following arguments:"
+	echo
+	echo "  $AUTOGEN_CONFIGURE_ARGS"
+	echo
+	echo "If you wish to pass additional arguments, please specify them "
+	echo "on the $0 command line."
+	echo
+    fi
 fi
 
-if ! aclocal $ACLOCAL_FLAGS; then
-   echo
-   echo "aclocal gave errors. Please fix the error conditions and try again."
-   exit 1
+if test -z "$ACLOCAL_FLAGS"; then
+
+    acdir=`$ACLOCAL --print-ac-dir`
+    m4list="pkg.m4"
+
+    for file in $m4list
+    do
+	if [ ! -f "$acdir/$file" ]; then
+	    echo
+	    echo "WARNING: aclocal's directory is $acdir, but..."
+            echo "         no file $acdir/$file"
+            echo "         You may see fatal macro warnings below."
+            echo "         If these files are installed in /some/dir, set the ACLOCAL_FLAGS "
+            echo "         environment variable to \"-I /some/dir\", or install"
+            echo "         $acdir/$file."
+            echo
+        fi
+    done
 fi
 
-automake --add-missing
-autoconf
+$ACLOCAL $ACLOCAL_FLAGS
+RC=$?
+if test $RC -ne 0; then
+   echo "$ACLOCAL gave errors. Please fix the error conditions and try again."
+   exit $RC
+fi
+
+$AUTOMAKE --add-missing || exit $?
+autoconf || exit $?
 
 cd $ORIGDIR
 
-if $srcdir/configure --enable-maintainer-mode "$@"; then
-  echo
-  echo "Now type 'make' to compile $PROJECT."
-else
+$srcdir/configure --enable-maintainer-mode $AUTOGEN_CONFIGURE_ARGS "$@"
+RC=$?
+if test $RC -ne 0; then
   echo
   echo "Configure failed or did not finish!"
+  exit $RC
 fi
+
+echo
+echo "Now type 'make' to compile $PROJECT."
